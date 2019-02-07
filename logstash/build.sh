e7ae6c18b4ea1eaee75956452dce63d24e27c1a5
Martin LEKPA <martin.lekpa@ext.saint-gobain.com>
2019-02-07 13:53:54 +0100

    initial commit

---
+++ logstash/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SGDSI_REPO="lekpamartin"
+
+TYPE="logstash-docker"
+TAG="6.6.0"
+
+docker build \
+        --no-cache \
+        -t ${SGDSI_REPO}/${TYPE}:"${TAG}" .
+
+if [ "$?" == 0 ]; then
+        echo -e "\n\t\t\t- status : ${SGDSI_REPO}/${TYPE}:${TAG} -> OK"
+        docker push ${SGDSI_REPO}/${TYPE}:${TAG}
+else
+        echo -e "\n\t\t\t- status : ${SGDSI_REPO}/${TYPE}:${TAG} -> KO"
+fi

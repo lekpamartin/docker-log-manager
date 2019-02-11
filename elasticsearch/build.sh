27734d8e0b4e0a8f7a0b42950bb629ab66127d3e
Martin LEKPA <martin.lekpa@ext.saint-gobain.com>
2019-02-11 16:50:28 +0100

    update

---
+++ elasticsearch/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+REPO="lekpamartin"
+
+TYPE="elasticsearch-docker"
+TAG="6.6.0"
+
+docker build \
+        --no-cache \
+        -t ${REPO}/${TYPE}:"${TAG}" .
+
+if [ "$?" == 0 ]; then
+        echo -e "\n\t\t\t- status : ${REPO}/${TYPE}:${TAG} -> OK"
+        docker push ${REPO}/${TYPE}:${TAG}
+else
+        echo -e "\n\t\t\t- status : ${REPO}/${TYPE}:${TAG} -> KO"
+fi
